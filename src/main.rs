c1af24e476ba600fed08f264823f23e062aa6414
Elizbeth <elizabethadebayo269@gmail.com>
2024-07-18 18:16:01 +0100

    generated random number = success

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+use rand::*;
 use std::io;
 
 fn main() {
-    println!("Guess the number? Pless input your guess");
+    println!("Guess the number? ");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("secret_number: {secret_number}");
+
+    println!("Pless input your guess");
     let mut guess = String::new(); //variable that stores user inputs
     io::stdin()
         .read_line(&mut guess)
@@ -12,3 +17,7 @@ fn main() {
 // let x = 5;
 // let y = 10;
 // println!("x: {x}, y: {}", y + 2);
+
+// let mut sec = thread_rng();
+// let blue = sec.gen_range(1..=5);
+// println!("blue: {blue}");
