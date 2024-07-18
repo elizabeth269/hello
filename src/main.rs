46a1518491606b1342056c19f241a571072ea587
Elizbeth <elizabethadebayo269@gmail.com>
2024-07-18 17:56:45 +0100

    ran some code

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,13 @@ use std::io;
 
 fn main() {
     println!("Guess the number? Pless input your guess");
-    let mut guess = String::new();
+    let mut guess = String::new(); //variable that stores user inputs
     io::stdin()
         .read_line(&mut guess)
         .expect("failed to read line");
     println!("you guessed {}", guess);
+
+    let x = 5;
+    let y = 10;
+    println!("x: {x}, y: {}", y + 2);
 }
