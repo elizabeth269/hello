306db2e13db055d7f74158cca8cc6cb40379d569
Elizbeth <elizabethadebayo269@gmail.com>
2024-07-18 18:02:26 +0100

    generating random numbers added my dependency "rand" cleanded up my code

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@ fn main() {
         .read_line(&mut guess)
         .expect("failed to read line");
     println!("you guessed {}", guess);
-
-    let x = 5;
-    let y = 10;
-    println!("x: {x}, y: {}", y + 2);
 }
+
+// let x = 5;
+// let y = 10;
+// println!("x: {x}, y: {}", y + 2);
