49b564c992402ba7a49a7a310ceb00dff76fb1ed
Elizbeth <elizabethadebayo269@gmail.com>
2024-07-18 18:26:48 +0100

    added a loop

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use rand::*;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
@@ -11,7 +12,16 @@ fn main() {
     io::stdin()
         .read_line(&mut guess)
         .expect("failed to read line");
+    let guess: u32 = guess.trim().parse().expect("please type a number");
     println!("you guessed {}", guess);
+
+    loop {
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("too small"),
+            Ordering::Greater => println!("too big"),
+            Ordering::Equal => println!("you win"),
+        }
+    }
 }
 
 // let x = 5;
