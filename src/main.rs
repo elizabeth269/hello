79ec05f3d5f266e1b38dca0c2e73a856fc267482
Elizbeth <elizabethadebayo269@gmail.com>
2024-07-18 17:35:20 +0100

    guessing game code inthe main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number? Pless input your guess");
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("failed to read line");
+    println!("you guessed {}", guess);
 }
