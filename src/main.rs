62634c3e3ebf06f033c7b46417c4f4e3f08beb27
Elizbeth <elizabethadebayo269@gmail.com>
2024-07-20 20:24:08 +0100

    panic

--- src/main.rs
+++ src/main.rs
@@ -148,14 +148,20 @@ fn main() {
     // for (key, value) in &scores {
     //     println!("{key}: {value}")
     // }
-    use std::collections::HashMap;
+    // use std::collections::HashMap;
 
-    let field_name = String::from("Favorite color");
-    let field_value = String::from("Blue");
+    // let field_name = String::from("Favorite color");
+    // let field_value = String::from("Blue");
 
-    let mut map = HashMap::new();
-    map.insert(field_name, field_value);
-    // field_name and field_value are invalid at this point, try using them and
-    // see what compiler error you get!
-    println!("{map:?}")
+    // let mut map = HashMap::new();
+    // map.insert(field_name, field_value);
+    // // field_name and field_value are invalid at this point, try using them and
+    // // see what compiler error you get!
+    // println!("{map:?}")
+
+    //panic
+    // panic!("crash and burn");
+    let v = vec![1, 2, 3];
+
+    v[99];
 }
