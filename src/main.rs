17d3b9af3b0290eb7311219f214aa9f20d4c5348
Elizbeth <elizabethadebayo269@gmail.com>
2024-07-20 09:51:40 +0100

    Accessing the score for the Blue team stored in the hash map

--- src/main.rs
+++ src/main.rs
@@ -139,9 +139,10 @@ fn main() {
 
     let mut scores = HashMap::new();
 
-    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Blue"), 0);
     scores.insert(String::from("Yellow"), 50);
 
     let team_name = String::from("Blue");
     let score = scores.get(&team_name).copied().unwrap_or(0);
+    println!("{score}");
 }
