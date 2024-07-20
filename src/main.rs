ed08abd62025e5a1f8220380e6310487adb23fd8
Elizbeth <elizabethadebayo269@gmail.com>
2024-07-20 09:58:46 +0100

    hashmaps and ownerships

--- src/main.rs
+++ src/main.rs
@@ -135,17 +135,27 @@ fn main() {
     // //updating a string
 
     // hash map
+    // use std::collections::HashMap;
+
+    // let mut scores = HashMap::new();
+
+    // scores.insert(String::from("Blue"), 0);
+    // scores.insert(String::from("Yellow"), 50);
+
+    // // let team_name = String::from("Blue");
+    // // let score = scores.get(&team_name).copied().unwrap_or(0);
+    // // println!("{score}");
+    // for (key, value) in &scores {
+    //     println!("{key}: {value}")
+    // }
     use std::collections::HashMap;
 
-    let mut scores = HashMap::new();
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
 
-    scores.insert(String::from("Blue"), 0);
-    scores.insert(String::from("Yellow"), 50);
-
-    // let team_name = String::from("Blue");
-    // let score = scores.get(&team_name).copied().unwrap_or(0);
-    // println!("{score}");
-    for (key, value) in &scores {
-        println!("{key}, {value}")
-    }
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+    // field_name and field_value are invalid at this point, try using them and
+    // see what compiler error you get!
+    println!("{map}")
 }
