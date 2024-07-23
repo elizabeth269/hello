18487949ac501c8d6234dc0fc825aa54742c97a2
Elizbeth <elizabethadebayo269@gmail.com>
2024-07-23 07:35:57 +0100

    test

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,14 +1,41 @@
-pub fn add(left: usize, right: usize) -> usize {
+// #[derive(Debug)]
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
+
+// impl Rectangle {
+//     fn can_hold(&self, other: &Rectangle) -> bool {
+//         self.width > other.width && self.height > other.height
+//     }
+// }
+pub fn add_two(left: usize, right: usize) -> usize {
     left + right
 }
-
 #[cfg(test)]
 mod tests {
     use super::*;
-
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn it_adds_two() {
+        let result = add_two(2, 4);
+        assert_eq!(result, 6);
     }
 }
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn larger_can_hold_smaller() {
+//         let larger = Rectangle {
+//             width: 8,
+//             height: 7,
+//         };
+//         let smaller = Rectangle {
+//             width: 5,
+//             height: 1,
+//         };
+
+//         assert!(larger.can_hold(&smaller));
+//     }
+// }
