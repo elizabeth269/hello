3d3f3a39ab2f8bdd350a2d1dd8250f872b1fd22f
Elizbeth <elizabethadebayo269@gmail.com>
2024-07-25 21:13:15 +0100

    lifetime annotations

--- src/main.rs
+++ src/main.rs
@@ -104,6 +104,14 @@
 
 // vectors
 fn main() {
+    let string1 = String::from("long string is long");
+
+    {
+        let string2 = String::from("xyz");
+        let result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {result}");
+    }
+
     // let mut v: Vec<i32> = Vec::new();
     // // updating a vector
     // v.push(5);
@@ -251,4 +259,11 @@ fn main() {
     //     Some(T),
     //     None,
     // }
+}
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
 }
